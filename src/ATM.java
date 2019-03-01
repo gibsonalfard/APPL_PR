4ca803b240c112d081c6184048561925b407842a
HamParasUt <hamzah.prasetio.tif417@polban.ac.id>
2019-03-01 08:22:06 +0700

    tambah nasabah
    menambah restore variable adminAuthenticate pada class ATM.
    Pada class BankDatabase membuat variabel penampung jml array accounts yang digunakan dr tersedia 100, menambah method pemeriksa avaiblelity account number, dan method penambah account.
    Membuat class AddNasabah yang menangani penambahan akun nasabah.

--- src/ATM.java
+++ src/ATM.java
@@ -39,6 +39,7 @@ public class ATM {
          
          performTransactions(); // user is now authenticated
          userAuthenticated = false; // reset before next ATM session
+         adminAuthenticated = false; // reset before next ATM session
          currentAccountNumber = 0; // reset before next ATM session
          screen.displayMessageLine("\nThank you! Goodbye!");
       }
