6467a46a77757b50da4c6d77ad1704758a9e8c22
ahmad aji <ahmadajinaufalali@gmail.com>
2019-03-10 20:31:19 +0700

    junit in progress v0.1

--- test/AccountTest.java
+++ test/AccountTest.java
@@ -43,13 +43,31 @@ public class AccountTest {
     //
      @Test
      public void LoginAccount() {
-         assertTrue("Login Gagal",bd.authenticateUser(1234, 4321));
-         assertFalse("Login Sukses",bd.authenticateUser(1234, 42321));
+         assert(bd.authenticateUser(1234, 4321));
+         assert(bd.authenticateUser(8765, 5678));
+         assert(bd.authenticateUser(00000, 00000));
+         assert(bd.authenticateUser(12345, 54321) == false);
+         assert(bd.authenticateUser(87612312, 51231236) == false);
+         assert(bd.authenticateUser(-87612312, -51231236) == false);
+         assert(bd.authenticateUser(-0, -1) == false);
+//         assert(bd.authenticateUser(-0, -0) == false); //EROR
+//         assertTrue("Login Gagal",bd.authenticateUser(1234, 4321));
+//         assertFalse("Login Sukses",bd.authenticateUser(1234, 42321));
      }
      
      @Test
      public void getBalance(){
-         assertEquals("Balance Tidak Sesuai", 999.5, bd.getAvailableBalance(1234), 0.5);
+         assert(bd.getAvailableBalance(1234) == 1000.0);
+         assert(bd.getAvailableBalance(8765) == 200.0);
+         assert(bd.getAvailableBalance(00000) == 200.0);
+         System.out.println(bd.getAvailableBalance(12345));
+         assert(bd.getAvailableBalance(12345) == 0.0);
+//           assert.assertNull(bd.getAvailableBalance(12345));
+//         assert(bd.getAvailableBalance(12345) == 0.0);
+//         assert(bd.getAvailableBalance(-10) = 0.0);
+         
+         
+//         assertEquals("Balance Tidak Sesuai", 999.5, bd.getAvailableBalance(1234), 0.5);
      }
      
      public void afterWithdrawal(){
@@ -62,7 +80,8 @@ public class AccountTest {
      public void afterDeposit(){
          afterWithdrawal();
          bd.debit(1234, 20);
-         
          assertEquals("AD : Total Tidak sesuai",1020.00, bd.getTotalBalance(1234), 0);
      }
+     
+     
 }
